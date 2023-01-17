8bbe9df5ec06f872b863dc4c63d03054c9244fee
JackpotPlus <xtremeglory@163.com>
2023-01-18 01:00:30 +0800

    调整List接口实现

--- src/com/xtremeglory/impl/List.java
+++ src/com/xtremeglory/impl/List.java
@@ -23,34 +23,21 @@ public interface List<E> {
     E remove(int index);
 
     /**
-     * 从开始索引位置(index),按照搜索顺序(search_direction),一直找到第一个匹配的元素,将其删除并返回该元素
-     *
-     * @param element          要查找的元素
-     * @param index            搜索的起始位置索引(包含该元素)
-     * @param search_direction 搜索的顺序
-     * @return 被删除的表中元素
-     */
-    E remove(E element, int index, SearchDirection search_direction);
-
-    /**
      * 从开始索引位置(index),向后一直找到第一个匹配的元素,将其删除并返回该元素
      *
      * @param element 要查找的元素
      * @param index   搜索的起始位置索引(包含该元素)
      * @return 被删除的表中元素
      */
-    default E remove(E element, int index) {
-        return this.remove(element, index, SearchDirection.FROM_FRONT_TO_BACK);
-    }
+    E remove(E element, int index);
 
     /**
-     * 从表头开始搜索并删除第一个匹配的元素
+     * 从表头开始,向后删除匹配的元素
      *
-     * @param element 要查找的元素
      * @return 被删除的元素
      */
     default E remove(E element) {
-        return this.remove(element, 0, SearchDirection.FROM_FRONT_TO_BACK);
+        return this.remove(element, 0);
     }
 
     /**
@@ -72,25 +59,13 @@ public interface List<E> {
     // 查找
 
     /**
-     * 从开始索引位置(index),按照搜索顺序(search_direction),找到第一个匹配的元素,返回其索引值
-     *
-     * @param element          要查找的元素
-     * @param index            搜索的起始位置
-     * @param search_direction 搜索方向
-     * @return 元素在表中存在 ? 元素的索引 : -1
-     */
-    int indexOf(E element, int index, SearchDirection search_direction);
-
-    /**
      * 从开始索引位置(index),向后找到第一个匹配的元素,返回其索引值
      *
      * @param element 要查找的元素
-     * @param index   搜索的起始位置
+     * @param index   搜索的起始位置(包含该元素)
      * @return 元素在表中存在 ? 元素的索引 : -1
      */
-    default int indexOf(E element, int index) {
-        return this.indexOf(element, index, SearchDirection.FROM_FRONT_TO_BACK);
-    }
+    int indexOf(E element, int index);
 
     /**
      * 从表头位置,向后找到第一个匹配的元素,返回其索引值
@@ -99,31 +74,17 @@ public interface List<E> {
      * @return 元素在表中存在 ? 元素的索引 : -1
      */
     default int indexOf(E element) {
-        return this.indexOf(element, 0, SearchDirection.FROM_FRONT_TO_BACK);
-    }
-
-    /**
-     * 从开始索引位置(index),按照搜索顺序(search_direction),查找元素是否存在
-     *
-     * @param element          要查找的元素
-     * @param index            搜索的起始位置
-     * @param search_direction 搜索方向
-     * @return 元素在表中是否存在
-     */
-    default boolean contains(E element, int index, SearchDirection search_direction) {
-        return this.indexOf(element, index, search_direction) != -1;
+        return this.indexOf(element, 0);
     }
 
     /**
      * 从开始索引位置(index),向后查找元素是否存在
      *
      * @param element 要查找的元素
-     * @param index   搜索的起始位置
+     * @param index   搜索的起始位置(包含该元素)
      * @return 元素在表中是否存在
      */
-    default boolean contains(E element, int index) {
-        return this.contains(element, index, SearchDirection.FROM_FRONT_TO_BACK);
-    }
+    boolean contains(E element, int index);
 
     /**
      * 从表头开始,向后查找元素是否存在
--- src/com/xtremeglory/impl/RaodomAccessList.java
+++ src/com/xtremeglory/impl/RaodomAccessList.java
@@ -2,7 +2,40 @@ package com.xtremeglory.impl;
 
 /**
  * 此接口是一个标记接口，任何一个实现该接口的数据结构都可视为一个可以随机存储的表
+ * 随机存储表可以以任意顺序访问元素
  * @param <E>
  */
 public interface RaodomAccessList<E> extends List<E> {
+    /**
+     * 从开始索引位置(index),按照搜索顺序(search_direction),一直找到第一个匹配的元素,将其删除并返回该元素
+     *
+     * @param element          要查找的元素
+     * @param index            搜索的起始位置索引(包含该元素)
+     * @param search_direction 搜索的顺序
+     * @return 被删除的表中元素
+     */
+    E remove(E element, int index, SearchDirection search_direction);
+
+    /**
+     * 从开始索引位置(index),按照搜索顺序(search_direction),找到第一个匹配的元素,返回其索引值
+     *
+     * @param element          要查找的元素
+     * @param index            搜索的起始位置(包含该元素)
+     * @param search_direction 搜索方向
+     * @return 元素在表中存在 ? 元素的索引 : -1
+     */
+    int indexOf(E element, int index, SearchDirection search_direction);
+
+
+    /**
+     * 从开始索引位置(index),按照搜索顺序(search_direction),查找元素是否存在
+     *
+     * @param element          要查找的元素
+     * @param index            搜索的起始位置(包含该元素)
+     * @param search_direction 搜索方向
+     * @return 元素在表中是否存在
+     */
+    default boolean contains(E element, int index, SearchDirection search_direction) {
+        return this.indexOf(element, index, search_direction) != -1;
+    }
 }
